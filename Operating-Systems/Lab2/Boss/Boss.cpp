1932306b97ae84173beb5115717734e1563a3223
Egor <60915234+egbusko@users.noreply.github.com>
2022-10-06 11:00:14 +0300

    Add files via upload

---
+++ Operating-Systems/Lab2/Boss/Boss.cpp
@@ -0,0 +1,127 @@
+// Boss.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include "pch.h"
+#include <windows.h>
+#include <iostream>
+
+
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	STARTUPINFO cif;
+	ZeroMemory(&cif, sizeof(STARTUPINFO));
+	PROCESS_INFORMATION pi;
+
+
+	char czCommandLine[] = "C:\\Egor\\Univer\\Програм\\Visual Stud\\source\\repos\\4 семестр\\C2\\Scout\\Debug\\Scout.exe";
+
+	int numScouts;
+	cout << "Enter number of Scounts: ";
+	cin >> numScouts;
+
+	DWORD dwEvent;
+	HANDLE e_ready = CreateEvent(NULL, FALSE, TRUE, "ready_to_work");
+	HANDLE e_dot = CreateEvent(NULL, FALSE, FALSE, "dot_entred");
+	HANDLE e_hyphen = CreateEvent(NULL, FALSE, FALSE, "minus_entered");
+	HANDLE e_other = CreateEvent(NULL, FALSE, FALSE, "other_symbol_entered");
+		HANDLE ghEvents[3];
+	ghEvents[0] = e_dot;
+	ghEvents[1] = e_hyphen;
+	ghEvents[2] = e_other;
+
+	HANDLE mutex_is_busy = CreateMutex(NULL, TRUE, "is_busy");
+
+
+
+
+
+	for (int i = 0; i < numScouts; i++)	{
+		if (!CreateProcess(czCommandLine, NULL,NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &cif, &pi) == TRUE) {
+			cout << "process, handle " << pi.hProcess << " " << GetLastError() << endl;					
+		}
+	}
+
+
+	bool tmp;
+	for (int i = 0; i < numScouts; i++) {
+		tmp = false;
+		SetEvent(e_ready);
+
+		
+		while (!tmp) {
+			dwEvent = WaitForMultipleObjects(3, ghEvents, FALSE, INFINITE);
+		
+			switch (dwEvent)
+			{
+			case WAIT_OBJECT_0 + 0:
+				cout << ".";
+				break;
+
+			case WAIT_OBJECT_0 + 1:
+				cout << "-";
+				break;
+
+			case WAIT_OBJECT_0 + 2:
+				cout << endl;
+				tmp = true;
+				break;
+				
+			default:
+				printf("Wait error: %d\n", GetLastError());
+				ExitProcess(0);
+			}
+		}
+	}
+	CloseHandle(e_ready);
+	CloseHandle(e_dot);
+	CloseHandle(e_hyphen);
+	CloseHandle(e_other);
+
+	system("pause");
+	return 0;
+}
+
+/*
+#include "pch.h"
+#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char lpszAppName[] = "C:\\ConsoleProcess.exe";
+	STARTUPINFO si;
+	PROCESS_INFORMATION piApp;
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	// создаем новый консольный процесс
+	if (!CreateProcess((LPCWSTR)"C:\\Egor\\Univer\\Програм\\Visual Stud\\source\\repos\\4 семестр\\Boss\\Debug\\Boss.exe", NULL, NULL, NULL, FALSE,
+		CREATE_NEW_CONSOLE, NULL, NULL, &si, &piApp))
+	{
+		_cputs("The new process is not created.\n");
+		_cputs("Check a name of the process.\n");
+		_cputs("Press any key to finish.\n");
+		_getch();
+		return 0;
+	}
+	_cputs("The new process is created.\n");
+	// ждем завершения созданного прцесса
+	WaitForSingleObject(piApp.hProcess, INFINITE);
+	// закрываем дескрипторы этого процесса в текущем процессе
+	CloseHandle(piApp.hThread);
+	CloseHandle(piApp.hProcess);
+	return 0;
+}
+*/
+//ready
+//.
+//-
+//any other symbol - end sending
+//end sending
---
+++ Operating-Systems/Lab2/Scout/Scout.cpp
@@ -0,0 +1,51 @@
+﻿// Scout.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include "pch.h"
+#include <windows.h>
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+	HANDLE e_ready = CreateEvent(NULL, FALSE, TRUE, "ready_to_work");
+	HANDLE e_dot = CreateEvent(NULL, FALSE, FALSE, "dot_entred");
+	HANDLE e_hyphen = CreateEvent(NULL, FALSE, FALSE, "minus_entered");
+	HANDLE e_other = CreateEvent(NULL, FALSE, FALSE, "other_symbol_entered");
+	HANDLE mutex_is_busy = CreateMutex(NULL, FALSE, "is_busy");
+
+
+	cout << "Enter your SOS message: ";
+
+	
+	
+	WaitForSingleObject(e_ready, INFINITE);
+
+	char str_entered = ' ';
+	
+	while (true) {
+		str_entered = _getch();
+		
+		if (str_entered == '.') {
+			SetEvent(e_dot);
+			cout << ".";
+		}
+		else if (str_entered == '-') {
+			SetEvent(e_hyphen);
+			cout << "-";
+		}
+		else {
+			SetEvent(e_other);
+			cout << "*other*" << endl;
+			break;
+		}
+	}
+	//ReleaseMutex(mutex_is_busy);
+
+
+	ExitProcess(0);
+
+	system("pause");
+	return 0;
+
+}
