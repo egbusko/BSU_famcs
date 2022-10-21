425900c2486986ef7be88851ef72a2f61cd726ca
Egor <60915234+egbusko@users.noreply.github.com>
2022-10-21 13:41:01 +0300

    Add files via upload

---
+++ Programming/Semester-2/MFC1_1-DlgDemo2/resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DlgDemo2.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DLGDEMO2_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_FILEIN                 1000
+#define IDC_BTN_GETFILEIN               1001
+#define IDC_EDIT_FILEOUt                1002
+#define IDC_BTN_GETFILEOUT              1003
+#define IDC_BUTTON3                     1004
+#define IDC_BTN_PROCESS                 1004
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
