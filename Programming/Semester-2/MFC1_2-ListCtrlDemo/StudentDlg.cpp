e2efa7a069ee3ae70871af0714d9e6ae5f05bbe0
Egor <60915234+egbusko@users.noreply.github.com>
2022-10-21 13:43:30 +0300

    Add files via upload

---
+++ Programming/Semester-2/MFC1_2-ListCtrlDemo/StudentDlg.cpp
@@ -0,0 +1,39 @@
+﻿// StudentDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "MFC2 (ListCtrlDemo).h"
+#include "StudentDlg.h"
+#include "afxdialogex.h"
+
+
+// CStudentDlg dialog
+
+IMPLEMENT_DYNAMIC(CStudentDlg, CDialog)
+
+CStudentDlg::CStudentDlg(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DLG_STUDENT_EDIT, pParent)
+	, m_sFIO(_T(""))
+	, m_fMark(0)
+{
+
+}
+
+CStudentDlg::~CStudentDlg()
+{
+
+}
+
+void CStudentDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_FIO, m_sFIO);
+	DDX_Text(pDX, IDC_EDIT_MARK, m_fMark);
+}
+
+
+BEGIN_MESSAGE_MAP(CStudentDlg, CDialog)
+END_MESSAGE_MAP()
+
+
+// CStudentDlg message handlers
